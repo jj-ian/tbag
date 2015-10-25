96e0ae69199d40945d0250b95d898aa10b8d2c79
Julie <julie.y.chien@gmail.com>
2015-10-25 19:11:30 -0400

    working on parser and ast

---
+++ tbag Compiler/ast.ml
@@ -0,0 +1,13 @@
+type func_decl = {
+		freturn: string;
+        fname : string;
+        formals : string list;
+        locals: string list;
+        body : stmt list;
+}
+
+let string_of_fdecl fdecl =
+  fdecl.fname ^ "(" ^ String.concat ", " fdecl.formals ^ ")\n{\n" ^
+  String.concat "" (List.map string_of_vdecl fdecl.locals) ^
+  String.concat "" (List.map string_of_stmt fdecl.body) ^
+  "}\n"
---
+++ tbag Compiler/parser.mly
@@ -0,0 +1,9 @@
+%{ open Ast %}
+
+prog:
+	decls EOF {$1}
+
+decls:
+   /* nothing */ { [], [] }
+ | decls vdecl { ($2 :: fst $1), snd $1 }
+ | decls fdecl { fst $1, ($2 :: snd $1) }
