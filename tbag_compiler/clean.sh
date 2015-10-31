cebf2d3c0d2c925e90e1aec77604c3f51895487c
Gregory Chen <gregorychen3@Gregorys-MacBook-Pro.local>
2015-10-30 21:13:21 -0400

    Modified clean.sh and make.sh to display commands being executed. Started test.sh

--- tbag_compiler/clean.sh
+++ tbag_compiler/clean.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
+set -x
 rm -f scanner.ml parser.ml parser.mli ast.cmo ast.cmi tbagInterpreter.cmo tbagInterpreter.cmi
+set +x
 
--- tbag_compiler/make.sh
+++ tbag_compiler/make.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -x
+
 ocamllex scanner.mll
 ocamlyacc parser.mly
 ocamlc -c ast.ml
+
+set +x
