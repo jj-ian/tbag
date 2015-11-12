177727d16c10d3c046ab6d6301fe58fdb0d55061
bslakter <bslakter@dyn-129-236-238-24.dyn.columbia.edu>
2015-11-11 19:25:29 -0500

    should be able to print

--- tbag_compiler/tbag.ml
+++ tbag_compiler/tbag.ml
@@ -1,5 +1,6 @@
 let _ = 
         let lexbuf = Lexing.from_channel stdin in
         let program = Parser.basic_program Scanner.token lexbuf in
-        let listing = JavaCode.string_of_prog (Compile.translate program) in
-        print_endline listing
+        Compile.translate program
+        (*let listing = JavaCode.string_of_prog (Compile.translate program) in
+        print_endline listing*)
