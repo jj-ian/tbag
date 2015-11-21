96165cf75903bf24a7b2d1f70dd7def99e933ef3
Julie <julie.y.chien@gmail.com>
2015-11-21 17:50:38 -0500

    items and stats

---
+++ Java Target/src/tbagJavaTarget/Item.java
@@ -0,0 +1,16 @@
+package tbagJavaTarget;
+
+public class Item {
+	private String name;
+	
+	public Item(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	
+
+}
