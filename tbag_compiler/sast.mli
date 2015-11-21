f12e6227fbd2626eacce21228c8d1203070666f4
bslakter <bslakter@Brians-MacBook-Pro.local>
2015-11-21 12:02:30 -0500

    simplify sast

--- tbag_compiler/sast.mli
+++ tbag_compiler/sast.mli
@@ -1,19 +1,3 @@
 open Ast
 
-type variable_type =
-        Int
-        | String
-        | Void
-        | Array of variable_type * int
-
 type expression = expr * variable_type
-
-type expr =
-        IntLiteral of int
-        | StrLiteral of string
-        | Id of string
-        | Assign of string * expr
-        | ArrayAssign of string * int * expr
-        | ArrayAccess of string * int
-        | Binop of expr * op * expr 
-        | Call of string * expr list
