4f15cca6d4ad9b61e8c4af9b690f8ad656885674
bslakter <bslakter@Brians-MacBook-Pro.local>
2015-11-21 11:27:02 -0500

    debugging, should be good tho

--- tbag_compiler/jast.mli
+++ tbag_compiler/jast.mli
@@ -2,18 +2,18 @@ open Sast
 
 type main_method = 
 {
+        mmethod: func_decl;
 		rdecls: room_decl list;
 		adecls: adj_decl list;
 		ndecls: npc_decl list;
 		idecls: item_decl list;
-		mbody: stmt list;
 }
 
 type driver_class = main_method *
 					func_decl list
 
-
 type program = driver_class *
 			   room_def *
 			   npc_def *
 			   item_def
+
--- tbag_compiler/sast.mli
+++ tbag_compiler/sast.mli
@@ -6,9 +6,6 @@ type variable_type =
         | Void
         | Array of variable_type * int
 
-type argument_decl =
-        Argument of variable_type * string
-
 type expression = expr * variable_type
 
 type expr =
@@ -20,67 +17,3 @@ type expr =
         | ArrayAccess of string * int
         | Binop of expr * op * expr 
         | Call of string * expr list
-
-type var_decl =
-        Var of variable_type * string
-        | VarInit of variable_type * string * expr
-
-type stmt =
-        Block of stmt list
-        | Expr of expr
-        | Return of expr
-        | If of expr * stmt * stmt
-        | While of expr * stmt
-
-type room_def = var_decl list
-
-type room_decl = 
-{
-        rname: string;
-        rbody: stmt list;
-}
-
-type adj_decl = string list
-       
-type func_decl = 
-{
-        freturntype: variable_type;
-        fname : string;
-        formals : argument_decl list; 
-        locals: var_decl list;
-        body : stmt list;
-}
-
-type npc_def = var_decl list
-
-type npc_decl = 
-{
-        nname: string;
-        nbody: stmt list;
-}
-
-type item_def = var_decl list
-
-type item_decl = 
-{
-        iname: string;
-        ibody: stmt list;
-}
-
-type basic_program = func_decl list
-
-type simple_program = room_decl list * 
-                      func_decl list
-
-type room_program = room_def *
-                    room_decl list * 
-                    func_decl list
-
-type program =  room_def * 
-                room_decl list * 
-                adj_decl list * 
-                npc_def *
-                npc_decl list * 
-                item_def *
-                item_decl list * 
-                func_decl list
