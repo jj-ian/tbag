4b0d834ed47d368db3e97aaf0946d3e8cbd7c2ba
gregorychen3 <gregorychen3@gmail.com>
2015-11-21 14:09:00 -0500

    Added trivial pretty_printer.ml, modified tbag.ml and Makefile accordingly.

---
+++ tbag_compiler/pretty_printer.ml
@@ -0,0 +1,2 @@
+let pretty_print (driver_class, room_def, npc_def, item_def) = 
+        "test"
--- tbag_compiler/tbag.ml
+++ tbag_compiler/tbag.ml
@@ -3,8 +3,11 @@ open Printf
 let _ = 
         let lexbuf = Lexing.from_channel stdin in
         let program = Parser.program Scanner.token lexbuf in
-        let program_text = Java_builder.rearrange program in
+        let jast_program = Java_builder.rearrange program in
+                Pretty_printer.pretty_print jast_program;
+                (*
 		printf "%s" program_text;   (* write something *)   
                 flush stdout;
+                *)
         (*let listing = JavaCode.string_of_prog (Compile.translate program) in
         print_endline listing*)
