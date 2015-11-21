62e5aafc7efb6fb34166cd58481f9e3749f1b27a
gregorychen3 <gregorychen3@gmail.com>
2015-11-21 15:59:09 -0500

    new hello world program is being received by pretty_printer

--- tbag_compiler/tbag.ml
+++ tbag_compiler/tbag.ml
@@ -4,7 +4,6 @@ let _ =
         let lexbuf = Lexing.from_channel stdin in
         let program = Parser.program Scanner.token lexbuf in
         let jast_program = Java_builder.rearrange program in
-        let test2 = print_string "jast completed!\n" in
                 Pretty_printer.pretty_print jast_program;
                 (*
 		printf "%s" program_text;   (* write something *)   
