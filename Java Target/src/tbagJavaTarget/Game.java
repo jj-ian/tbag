8b1e88a4923a38bde9d4431258e207a23cd499d6
Julie <julie.y.chien@gmail.com>
2015-11-21 20:10:48 -0500

    support for items, picking up items from rooms, items i/o

--- Java Target/src/tbagJavaTarget/Game.java
+++ Java Target/src/tbagJavaTarget/Game.java
@@ -1,11 +1,13 @@
 package tbagJavaTarget;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Game {
+	private HashMap<String, Integer> playerStats;
+	private ArrayList<Item> playerItems;
 
-	//testing
 	private Room startingRoom;
 	private Scanner scanner;
 	
@@ -13,6 +15,9 @@ public class Game {
 	// user enters "3", we go to room "Dungeon", in this map will be a key, value
 	// pair where key = 3, value = Dungeon
 	private HashMap<Integer, Room> inputRoomMap;
+	
+	// stores what user input corresponds w/ which item
+	private HashMap<Integer, Item> inputItemMap;
 	
 	public static void main(String[] args) {
 		
@@ -31,6 +36,9 @@ public class Game {
 		// initialize other fields
 		scanner = new Scanner(System.in);
 		inputRoomMap = new HashMap<Integer, Room>();
+		inputItemMap = new HashMap<Integer, Item>();
+		playerStats = new HashMap<String, Integer>();
+		playerItems = new ArrayList<Item>();
 	}
 	
 	// returns a sample Game object that you can use for gameplay
@@ -47,41 +55,68 @@ public class Game {
 		bedroom.addAdjacentRoom(kitchen);
 		bedroom.addAdjacentRoom(outside);
 		bedroom.addAdjacentRoom(bathroom);
-		
 		bathroom.addAdjacentRoom(kitchen);
-		
 		outside.addAdjacentRoom(mall);
 		
-		return new Game(bedroom);
+		// add items to the rooms
+		Item coolShirt = new Item("Drake shirt", "A nice looking shirt with Drake on it.", null);
+		bedroom.addItem(coolShirt);
+		
+		Item knife = new Item("Knife", "A dull kitchen knife.", null);
+		kitchen.addItem(knife);
+		
+		Item spaghetti = new Item("Spaghetti", "A big pile of saucy spaghetti.", null);
+		kitchen.addItem(spaghetti);
+
+		// create a new game, setting bedroom as the starting room
+		Game game = new Game(bedroom);
+
+		// initialize programmer defined stats
+		game.playerStats.put("Health", new Integer(10));
+		game.playerStats.put("Strength", 2);
+		game.playerStats.put("Hunger", 5);
+		game.playerStats.put("Charisma", 3 );
+		
+		return game;
 	}
 	
+
 	// gameplay happens here
 	public void runGame() {
 		Room currentRoom = this.startingRoom;
-		int adjacentCounter = 1;
+		//int adjacentCounter = 1;
+		//int itemCounter = 1;
 		
 		while (/* user doesn't quit and exit condition is unsatisfied*/ true) {
 			System.out.println("Current location: " + currentRoom.getName());
 			System.out.println(currentRoom.getWelcomeMessage() + "\n");
-		
+			
+			//dealing w/ items in the room
+			handleItems(currentRoom);
+			
+			// dealing w/ going to other rooms
+			currentRoom = handleGoingToOtherRooms(currentRoom);
+			
+			/*
+			//going to other rooms
 			System.out.println("Where do you want to go?");
 			
 			for (Room adjacentRoom : currentRoom.adjacentRooms) {
 				System.out.println(adjacentCounter + " : " + adjacentRoom.getName());
-				this.inputRoomMap.put(new Integer(adjacentCounter), adjacentRoom);
+				inputRoomMap.put(new Integer(adjacentCounter), adjacentRoom);
 				adjacentCounter++;
 			}
 			
 			//TODO check for input type errors
-			int inputInt = this.scanner.nextInt();
+			int inputInt = scanner.nextInt();
 			System.out.println("You entered " + inputInt);
 			
 			Room roomToGoTo;
 			
 			// check that the number they entered maps to a valid room
-			while ((roomToGoTo = this.inputRoomMap.get(inputInt)) == null) {
+			while ((roomToGoTo = inputRoomMap.get(inputInt)) == null) {
 				System.out.println(inputInt + " is not a valid option.");
-				inputInt = this.scanner.nextInt();
+				inputInt = scanner.nextInt();
 			}
 			
 			System.out.println("Going to room " + roomToGoTo.getName());
@@ -93,8 +128,95 @@ public class Game {
 			// reset 
 			adjacentCounter = 1;
 			this.inputRoomMap.clear();
-			
+			*/
 		}
+	}
+	
+	public void handleItems(Room currentRoom) {
+		int itemCounter = 2;
+		
+		if (currentRoom.items.size() > 0) {
+			System.out.println("There are several items here. Which do you want to take?");
+
+			//add additional option for "pick up no items"
+			System.out.println("1 : Pick up nothing.");
+			
+			for (Item item : currentRoom.items) {
+				System.out.println(itemCounter + " : " + item.name + " -- " + item.description);
+				inputItemMap.put(new Integer(itemCounter), item);
+				itemCounter++;
+			}
+
+			int inputInt = scanner.nextInt();
+			//System.out.println("You entered " + inputInt);
+
+			// input 1 is for picking up no items. Check that the input isn't 1 before proceeding
+			if (inputInt == 1) {
+				System.out.println("You decided not to pick up anything.\n");
+			} else {
+				Item itemToPickUp;
+
+				// check that the number they entered maps to a valid item. If not, reprompt.
+				while ((itemToPickUp = inputItemMap.get(inputInt)) == null) {
+					System.out.println(inputInt + " is not a valid option.");
+					inputInt = scanner.nextInt();
+					
+					// if user enters 1 in the middle, they decided to not pick up anything. 
+					// exit the loop
+					if (inputInt ==1) {
+						System.out.println("You decided not to pick up anything.\n");
+						break;
+					}
+				}
+				
+				// If they picked up something, remove the item from the room
+				if (itemToPickUp != null) {
+					System.out.println("Picked up " + itemToPickUp.name + ".\n");
+					currentRoom.removeItem(itemToPickUp);
+				}
+			}
+
+			// reset 
+			itemCounter = 1;
+			inputItemMap.clear();
+		}
+	}
+	
+	// handles i/o for deciding which room to go to, and returns the room to go to
+	public Room handleGoingToOtherRooms(Room currentRoom) {
+		int adjacentCounter = 1;
+		System.out.println("Where do you want to go?");
+		
+		for (Room adjacentRoom : currentRoom.adjacentRooms) {
+			System.out.println(adjacentCounter + " : " + adjacentRoom.getName());
+			inputRoomMap.put(new Integer(adjacentCounter), adjacentRoom);
+			adjacentCounter++;
+		}
+		
+		//TODO check for input type errors
+		int inputInt = scanner.nextInt();
+		System.out.println("You entered " + inputInt);
+		
+		Room roomToGoTo;
+		
+		// check that the number they entered maps to a valid room
+		while ((roomToGoTo = inputRoomMap.get(inputInt)) == null) {
+			System.out.println(inputInt + " is not a valid option.");
+			inputInt = scanner.nextInt();
+		}
+		
+		System.out.println("Going to room " + roomToGoTo.getName());
+		System.out.println("--------------------------------------------");
+		
+		// re assign
+		//currentRoom = roomToGoTo;
+		
+		// reset 
+		adjacentCounter = 1;
+		this.inputRoomMap.clear();
+		
+		// return the room to go to
+		return roomToGoTo;
 	}
 
 }
--- Java Target/src/tbagJavaTarget/Item.java
+++ Java Target/src/tbagJavaTarget/Item.java
@@ -1,14 +1,39 @@
 package tbagJavaTarget;
 
+import java.util.HashMap;
+
 public class Item {
-	private String name;
+	public String name;
+	public String description;
+	public EquipFunction equipFunction;
 	
-	public Item(String name) {
+	public Item(String name, String description, EquipFunction equipFunction) {
 		this.name = name;
+		this.description = description;
+		this.equipFunction = equipFunction;
 	}
 	
-	public String getName() {
-		return name;
+	
+	/*
+	public void equip(HashMap<String, Integer> playerStats) {
+		//alter playerstats in some programmer defined way
+		
+		
+		
+	}*/
+	
+	public void equip(HashMap<String, Integer> playerStats, EquipFunction ef) {
+		//alter playerstats in some programmer defined way
+		ef.equipFunction(playerStats);
+		System.out.println(playerStats);
+		
+		
+	}
+	
+	
+	@Override
+	public String toString() {
+		return name + " -- " + description;
 	}
 	
 	
