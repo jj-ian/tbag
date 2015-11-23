ef895f7ca5ea9d86a189b5ce4831994b6190faee
Iris <iz2140@columbia.edu>
2015-11-22 20:04:56 -0500

    Still hacking away at semantic checker

--- tbag_compiler/semantic_checker.ml
+++ tbag_compiler/semantic_checker.ml
@@ -1,12 +1,12 @@
 open Ast
 open Sast
-open Lexing
+(*open Lexing*)
 open Map
 
 
 (*Could implement as Stringmap or hash *)
 type symbol_table = {
-(* again MV's group had these be mutable *)
+(*  MV's group had these be mutable *)
 	mutable parent : symbol_table option;
 	mutable variables : (string * checked_var_decl * var_types) list;
 	mutable functions : function_decl list;
@@ -15,7 +15,7 @@ type symbol_table = {
 	(*  add Rooms, Items, NPCs here*)
 }
 
-let rec find_variable (scope: symbol_table) name = 
+let rec check_id (scope: symbol_table) name = 
 	try 
 		List.find (fun (s, _, _, _ ) -> s = name ) scope.variables
 	with Not_found ->
@@ -30,36 +30,116 @@ type translation_environment = {
 	found_main: bool; 
 }
 
-let rec expr env = function 
-
+(* adapted from edwards' notes*)
+let rec check_expr (scope : symbol_table) (expr : Ast.expr) = match expr with
+	Noexpr -> Sast.Noexpr, Void
 	(*) An integer constant: convert and return Int type *)
-	Ast.IntLiteral(v) -> Sast.IntLiteral(v), variable_type.Int (* Or the last one should be just Int, not sure of the semantic notation.*)
+	| IntLiteral(v) -> Sast.IntLiteral(v), variable_type.Int (* Or the last one should be just Int, not sure of the semantic notation.*)
+	(*what is Sast.IntLiteral?? how does it know what type it is? What is this line really doing?*)
+
+	| String_literal(str) -> Sast.StrConst(str), Sast.String
 	
 	(* An identifier: verify it is scope and return its type *)
-	| Ast.Id(vname) -> 
+	| Id(vname) -> 
 		let vdecl = 
-			try find_variable env.scope vname (* locate a variable by name *)
+			try check_id scope vname (* locate a variable by name *)
 		with Not_found -> 
 			raise (Error("undeclared identifier " ^ vname))
 		in let (_, typ) = vdecl in (* get the variable's type *)
 		Sast.Id(vdecl), typ
 
+(*MV's groups check_exp below
+let rec check_expr (scope : symbol_table) (expr : Ast.expr) = match expr with
+	Noexpr -> Sast.Noexpr, Void
+	| Id(str) -> 
+		(try 
+			let (decl, t) = check_id scope str in Sast.Id(decl), t 
+		with Not_found -> raise (Failure ("Id named " ^ str ^ " not found")))
+	| Integer_literal(i) -> Sast.IntConst(i), Sast.Int
+	| String_literal(str) -> Sast.StrConst(str), Sast.String
+	| Boolean_literal(b) -> Sast.BoolConst(b), Sast.Boolean
+	| Array_access(_, _) as a -> check_array_access scope a
+	| Assign(_, _) as a -> check_assign scope a
+	| Uniop(op, expr) as u -> check_uni_op scope u
+	| Binop(_, _, _) as b -> check_op scope b
+	| Call(_, _) as c -> check_call scope c
+	| Access(_, _) as a -> check_access scope a
+	| Struct_Member_Assign(_, _, _) as a -> check_struct_assignment scope a
+	| Array_Member_Assign(_, _, _) as a -> check_array_assignment scope a
+*)
+
+let rec check_var_type (scope : symbol_table) (v : Ast.var_types) = match v with
+	Ast.Void -> Sast.Void
+	| Ast.Int -> Sast.Int
+	| Ast.String -> Sast.String
+	| Ast.Array(v, expr) ->
+		let v = check_var_type scope v in
+		let expr = check_expr scope expr in
+		let (_, t) = expr in
+		if t <> Int then raise (Error "Array size must have integer.")
+		else Sast.Array(v, expr) 
+
+let check_func_decl (env : translation_environment) (f : Ast.func_decl) =
+	let scope' = { env.scope with parent = Some(env.scope); variables = []; return_found = false } in
+	let t = check_var_type env.scope f.ftype in
+	
+	let formals = List.fold_left ( (* formals = arguments*)
+		fun a f -> match f with
+		Ast.Param(t, n) ->
+			let t = check_var_type scope' t in 
+			scope'.variables <- (n, Sast.Var(t, n), t) :: scope'.variables; (Sast.Var(t, n), t) :: a
+	) [] f.formals in
+	(*local variables*)
+	let locals = List.fold_left ( fun a l -> process_var_decl scope' l :: a ) [] f.locals in
+	(* currently not handling statements, units, or return type *)
+	(*let statements = process_func_stmt scope' f.body t in *)
+	(*let units = List.fold_left ( fun a u -> process_func_units scope' u formals t :: a) [] f.units in*)
+	(*if scope'.return_found then *)
+		let f = { 	ftype = t; 
+					fname = f.fname; 
+					checked_formals = formals; 
+					checked_locals = locals; 
+					(*checked_body = statements; 
+					checked_units = units*) } in
+		env.scope.functions <- f :: env.scope.functions; (* throw away scope of function *) f
+	(*else (if f.ftype = Void then 
+		let f = { 	ftype = t; 
+					fname = f.fname; 
+					checked_formals = formals; 
+					checked_locals = locals; 
+					checked_body = statements; 
+					checked_units = units } in
+		env.scope.functions <- f :: env.scope.functions; (* throw away scope of function *) f*)
+	(*else
+		raise (Failure ("No return for function " ^ f.fname ^ " when return expected.")))*)
 
 type function_table = {
 	funcs : func_decl list
 }
 
+let find_func (l : function_decl list) f =
+	List.find(fun c -> c.fname = f) l
+
+let process_func_decl (env : translation_environment) (f : Ast.func_decl) =
+	try
+		let _ = find_func env.scope.functions f.fname in
+			raise (Failure ("Function already declared with name " ^ f.fname))
+	with Not_found ->
+		if f.fname = "print" then raise (Failure "A function cannot be named 'print'")
+		else
+			check_func_decl env f
 
 let process_var_decl (scope : symbol_table) (v : Ast.var_decl) =
 	let triple = match v with
-		Variable(t, name) ->
+		Var(t, name) ->
 			let t = check_var_type scope t in 
-			(name, Sast.Variable(t, name), t)
-		| Variable_Initialization(t, name, expr) ->
+			(name, Sast.Var(t, name), t)
+		| VarInit(t, name, expr) ->
 			let t = check_var_type scope t in
 			let expr = check_expr scope expr in
 			let (_, t2 ) = expr in
-			if t <> t2 then raise (Failure "wrong type for variable initialization") else (name, Sast.Variable_Initialization(t, name, expr), t) 
+			if t <> t2 then raise (Failure "wrong type for variable initialization") 
+			else (name, Sast.VarInit(t, name, expr), t) (*return this*)
 
 	let (_, decl, t) = triple in
 	if t = Void then
@@ -69,10 +149,10 @@ let process_var_decl (scope : symbol_table) (v : Ast.var_decl) =
 		(decl, t))
 
 	
-let check_simple_program (p : Ast.program) =
+let check_basic_program (p : Ast.program) =
 	let s = { parent = None; variables = []; functions = []; return_found = false } in
 	let env = { scope = s; found_main = false } in
-	let (vars, funcs) = p in 	
+	let funcs = p in 	
 	let funcs = 
 		List.fold_left (
 			fun a f -> process_func_decl env f :: a
