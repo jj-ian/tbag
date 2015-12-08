2df5586eafd7b4088b846bbb214ff5100aff8bb5
Julie <julie.y.chien@gmail.com>
2015-12-07 20:33:14 -0500

    finished java target sample, refactored stuff to what generated code should look like, debugged sample, added mockup for cheat code support

--- Java Target/EventBasedJavaTarget/src/Driver.java
+++ Java Target/EventBasedJavaTarget/src/Driver.java
@@ -15,7 +15,6 @@ public class Driver {
 	public static boolean haveKey = false;
 	public static boolean room3Locked = true;
 	public static boolean tryToOpenLockedDoor = false;
-	public static boolean finishedLockPrompting = false;
 	
 
 	public static void main(String[] args) {
@@ -42,50 +41,66 @@ public class Driver {
 		// items
 		
 		while (true) {
+			
+			/* designate starting room */
 			if (!started) {
 				started = true;
 				movePlayerToRoom(room2);
+			}
+			
+			
+			/* ROOM 2 BEGINS --------------------------------------------- */
+			/* boilerplate code for room2 BEGINS ------------------------- */
+			
+			if (currentRoom == room2) {
 				System.out.println("which room?");
 				promptForInput(new String[]{"south", "east"});
-
 			}
 			
 			if (currentRoom == room2 && input.equalsIgnoreCase("south")) {
 				movePlayerToRoom(room1);
-				if (haveKey == false) {
-					System.out.println("there's a key here. pick it up?");
-					promptForInput(new String[]{"yes", "no"});
-				} 
-				else {
-					System.out.println("which room?");
-					promptForInput(new String[]{"north"});
-				}
 				
-			}
-			
-			
-			if (currentRoom == room1 && input.equalsIgnoreCase("yes")) {
-				System.out.println("you picked up key");
-				haveKey = true;
-				System.out.println("which room?");
-				promptForInput(new String[]{"north"});
-			}
-			
-			if (currentRoom == room1 && input.equalsIgnoreCase("no")) {
-				System.out.println("which room?");
-				promptForInput(new String[]{"north"});
-			}
-			
-			if (currentRoom == room1 && input.equalsIgnoreCase("north")) {
-				movePlayerToRoom(room2);
-				System.out.println("which room?");
-				promptForInput(new String[]{"south", "east"});
 			}
 			
 			if (currentRoom == room2 && input.equalsIgnoreCase("east")) {
 				movePlayerToRoom(room3);
 			}
+			
+			/* boilerplate code for room2 ENDS -------------------------- */
+			/* ROOM 2 ENDS ---------------------------------------------- */
+			
+			
+			/* ROOM 1 BEGINS --------------------------------------------- */
+			/* unique code for room1 BEGINS ------------------------------ */
+			
+			if (currentRoom == room1 && haveKey == false) {
+				System.out.println("there's a key here. pick it up?");
+				promptForInput(new String[]{"yes", "no"});
+			}
+			
+			if (currentRoom == room1 && input.equalsIgnoreCase("yes")) {
+				System.out.println("you picked up key");
+				haveKey = true;
+			}
+			
+			/* unique code for room1 ENDS -------------------------------- */
+			
+			/* boilerplate code for room1 BEGINS ------------------------- */
+			
+			if (currentRoom == room1) {
+				System.out.println("which room?");
+				promptForInput(new String[]{"north"});			}
+			
+			if (currentRoom == room1 && input.equalsIgnoreCase("north")) {
+				movePlayerToRoom(room2);
+			}
+			
+			/* boilerplate code for room1 ENDS -------------------------- */
+			/* ROOM 1 ENDS ---------------------------------------------- */
 
+			
+			/* ROOM 3 BEGINS --------------------------------------------- */
+			/* unique code for room3 BEGINS ------------------------------ */
 			
 			if (currentRoom == room3 && room3Locked == true) {
 				System.out.println("there's a locked door here");
@@ -93,7 +108,7 @@ public class Driver {
 			}
 			
 			if (currentRoom == room3 && room3Locked == false) {
-				System.out.println("the door is unlcoked");
+				System.out.println("the door is unlocked");
 				
 			}
 			
@@ -106,8 +121,6 @@ public class Driver {
 			if (currentRoom == room3 && tryToOpenLockedDoor == true && haveKey == false) {
 				System.out.println("u don't have the key");
 				tryToOpenLockedDoor = false;
-
-
 			}
 			
 			if (currentRoom == room3 && tryToOpenLockedDoor && input.equalsIgnoreCase("yes")) {
@@ -123,7 +136,20 @@ public class Driver {
 
 			}
 			
-			
+			/* boilerplate code behavior for room3 is overridden by the programmer here. 
+			 * Usually here it would prompt player for all adjacent rooms, but the programmer 
+			 * needs to override that here because one of the adjacent rooms may be locked.
+			 * Then, there is no boilerplate code for room3; it's all unique.
+			 * 
+			 * So to make this easy for programmer on the tbag side, we should have a library function 
+			 * called "prompt_for_adjacent_rooms(room3)" that programmer can call at the end of 
+			 * their room3 code. prompt_for_adjacent_rooms(room3) will generate all boilerplate 
+			 * move-to-adjacent-room code for room3.
+			 * 
+			 * If programmer does not want the default move-to-adjacent-room behavior,
+			 * they can simply replace the prompt_for_adjacent_rooms() call with their own 
+			 * handlers instead.
+			 */
 			if (currentRoom == room3) {
 				System.out.println("which ROOM?");
 				if (room3Locked) {
@@ -135,11 +161,8 @@ public class Driver {
 				
 			}
 
-			
 			if (currentRoom == room3 && input.equalsIgnoreCase("west")) {
-				movePlayerToRoom(room2);
-				System.out.println("which room?");
-				promptForInput(new String[]{"south", "east"});			
+				movePlayerToRoom(room2);		
 			}
 			
 			if (currentRoom == room3 && input.equalsIgnoreCase("east")) {
@@ -147,16 +170,45 @@ public class Driver {
 	
 			}
 			
+			/* unique code for room3 ENDS -------------------------------- */
+			/* ROOM 3 ENDS ---------------------------------------------- */
+
+			
+			/* ROOM 4 BEGINS --------------------------------------------- */
+			/* unique code for room4 BEGINS ------------------------------ */
+			// this demonstrates how a cheat code can be implemented
 			if (currentRoom == room4) {
-				System.out.println("whichhhhhh room?");
-				promptForInput(new String[]{"east"});		
+				System.out.println("how r u today?");
+				
+
+				System.out.println("available options: ");
+				System.out.println("good");
+				System.out.println("bad");
+
+
+				// this call of the special 2-argument promptForInput() will prompt the user
+				// for input, but not print out the available inputs. This allows for the 
+				// cheat code "xyzzy" to be an allowed input, but not be displayed to player.
+				promptForInput(new String[]{"good", "bad", "xyzzy"}, false);
+	
 			}
 			
-			if (currentRoom == room4 && input.equalsIgnoreCase("east")) {
+			if (currentRoom == room4 && input.equalsIgnoreCase("xyzzy")) {
+				System.out.println("SICK!!!!!!!!!!! U FOUND THE SPECIAL CHEAT!! IT TELEPORTS U BACK TO ROOM1!!");
+				movePlayerToRoom(room1);
+			}
+			
+			/* boilerplate code for room4 BEGINS ------------------------- */
+			if (currentRoom == room4) {
+				System.out.println("which room?");
+				promptForInput(new String[]{"west"});		
+			}
+			
+			if (currentRoom == room4 && input.equalsIgnoreCase("west")) {
 				movePlayerToRoom(room3);
 			}
-			
-			
+			/* boilerplate code for room4 ENDS -------------------------- */
+			/* ROOM 4 ENDS ---------------------------------------------- */
 			
 			
 		}
@@ -172,8 +224,11 @@ public class Driver {
 		
 	}
 	
-	// prompts player for input and sets global var "input" to whatever player inputted, provided it's a valid input.
-	// invalid inputs will be reprompted
+	// Prompts player for input and sets global var "input" to whatever player submitted, provided it's a valid input. 
+	// If invalid inputs are entered, it'll reprompt until player enters a valid input.
+	
+	// Arguments:
+	// String[] acceptableInputs -- the list of acceptable inputs
 	// boolean printAvailableInputs -- if true, it'll print the list of possible inputs. if false, it won't. enter false if you 
 	// want to provide functionality like cheat codes -- where there are valid inputs that you don't want to display to player
 	public static void promptForInput(String[] acceptableInputs, boolean printAvailableInputs) {
@@ -189,6 +244,7 @@ public class Driver {
 			acceptableInputs[i] = acceptableInputs[i].toUpperCase();
 		}
 
+		// loop until player enters valid input
 		input = scanner.nextLine();
 		while(!Arrays.asList(acceptableInputs).contains(input.toUpperCase())) {
 			System.out.println("invalid input");
