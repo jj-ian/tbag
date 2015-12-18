1490edc96620c73ef1196df862dc8051dde03dc1
Maria van Keulen <mv2482@columbia.edu>
2015-12-18 08:53:59 -0500

    java_tbag outputs compiler error messages if fail

--- tbag_compiler/java_tbag.sh
+++ tbag_compiler/java_tbag.sh
@@ -4,14 +4,21 @@ basename=`echo $1 | sed 's/.*\\///
                          s/.tbag//'`
 inputtestsdirectory="tests/"
 
- ./tbag < $1
-javac Driver.java
+ ./tbag < $1 2> ${basename}_compiler_output.txt
 
-if [[ ${basename} = *"_input"* ]]
-then
-    java Driver < "$inputtestsdirectory"${basename}.in
-else
-    java Driver
-fi
+ if [$? -eq 0] then
+     rm ${basename}_compiler_output.txt
+
+     javac Driver.java
+
+     if [[ ${basename} = *"_input"* ]]
+     then
+         java Driver < "$inputtestsdirectory"${basename}.in
+     else
+         java Driver
+     fi
+ else
+     cat ${basename}_compiler_output.txt
+
 
 rm Driver.java Item.java Npc.java Room.java *.class
